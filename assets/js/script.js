fd1ddc2a015355f112ee123eecc776c2af6b6a92
Jenniffer <jenlpac@yahoo.com>
2020-11-05 10:50:00 -0600

    update ids for time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,29 +18,40 @@ $(document).ready(function() {
     })
 
     // Get data from localStorage
-    $("#8am .description").val(localStorage.getItem("8am"));
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("#10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+    $("#8-am .description").val(localStorage.getItem("8-am"));
+    $("#9-am .description").val(localStorage.getItem("9-am"));
+    $("#10-am .description").val(localStorage.getItem("10-am"));
+    $("#11-am .description").val(localStorage.getItem("11-am"));
+    $("#12-pm .description").val(localStorage.getItem("12-pm"));
+    $("#1-pm .description").val(localStorage.getItem("1-pm"));
+    $("#2-pm .description").val(localStorage.getItem("2-pm"));
+    $("#3-pm .description").val(localStorage.getItem("3-pm"));
+    $("#4-pm .description").val(localStorage.getItem("4-pm"));
+    $("#5-pm .description").val(localStorage.getItem("5-pm"));
 
     // Audit time to update section styling
     function auditTime() {
         // Set current time
+        var now = moment().hour();
+        console.log(now);
 
         // Loop through time blocks
+        $(".time-block").each(function () {
+            // Set variable for each time block
+            var timeBlock = parseInt($(this).attr("id").split(".")[1]);
+            
+                if(timeBlock < now) {
+                    // Style variable time block for past
+                    
+                    // Style variable time block for present
 
-        // Set variable for each time block
+                    // Style variable time block for future
+                }
+            
+        })
 
-        // Style variable time block for past
-
-        // Style variable time block for present
-
-        // Style variable time block for future
+        
     }
+
+    auditTime();
 })
