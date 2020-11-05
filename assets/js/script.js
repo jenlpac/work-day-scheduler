e3d00299594f4be8a0ad9baefbe8b8a896c03ea9
Jenniffer <jenlpac@yahoo.com>
2020-11-05 09:42:40 -0600

    save event data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,19 @@ var today = moment().format('dddd, MMMM Do, YYYY');
 $("#currentDay").html(today);
 
 // Save event data on button click
+$(document).ready(function() {
+    
+    // Save button was clicked
+    $(".saveBtn").click(function () {
+        // Get event and time values
+        var event = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        console.log(event);
+        console.log(time);
+
+        // Save event and time values
+        localStorage.setItem(event, time);
+    })
+
+
+})
